13be81ab861bfe97e84c5d8a3ca580ac55d9dfd1
tuva9 <tuva9@fpt.edu.vn>
2022-07-17 22:03:41 +0700

    Feature: Upload image

--- src/product.js
+++ src/product.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const {cloudinary} = require('../ultilities/cloudinary')
 
 
 router.get('/', async function(req, res) {
@@ -10,6 +11,18 @@ router.get('/', async function(req, res) {
         })
     } catch(err) {
         return res.status(500).send("Server error")
+    }
+})
+
+router.post('/upload', async function(req, res) {
+    try {
+        const fileStr = req.body.data
+        const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+            upload_preset: "anhhtus"
+        })
+        res.send(uploadResponse)
+    } catch(err) {
+        return res.status(500).send(err)
     }
 })
 
