638a5017904dbccd2dd8691e29a6cbe45ece04c5
tuva9 <tuva9@fpt.edu.vn>
2022-07-17 22:02:26 +0700

    Feature: Fixed CORS

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = require('./src/product')
+const cors = require('cors');
 
 const app = express()
-app.use(express.json())
+app.use(cors());
+app.use(express.json({limit: '0.1mb'}))
+app.use(express.urlencoded({limit: '0.1mb', extended: true}))
 app.use('/api', router)
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3002
 
 app.listen(port, function() {
     console.log("server run on port: ", port)
