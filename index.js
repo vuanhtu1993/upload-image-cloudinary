7f1bc707000069962a1aef8cd3f601fc2d832867
tuva9 <tuva9@fpt.edu.vn>
2022-07-17 22:09:42 +0700

    Change: image limit size

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 
 const app = express()
 app.use(cors());
-app.use(express.json({limit: '0.1mb'}))
-app.use(express.urlencoded({limit: '0.1mb', extended: true}))
+app.use(express.json({limit: '0.2mb'}))
+app.use(express.urlencoded({limit: '0.2mb', extended: true}))
 app.use('/api', router)
 
 const port = process.env.PORT || 3002
