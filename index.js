d8dfba11d727b6f8630f1a2e961130c29afd57b7
tuva9 <tuva9@fpt.edu.vn>
2022-08-04 16:20:07 +0700

    feature: download file

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const router = require('./src/product')
+const product = require('./src/product')
 const cors = require('cors');
 
 const app = express()
 app.use(cors());
 app.use(express.json({limit: '0.2mb'}))
 app.use(express.urlencoded({limit: '0.2mb', extended: true}))
-app.use('/api', router)
+app.use('/api', product)
 
 const port = process.env.PORT || 3002
 
--- src/product.js
+++ src/product.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const {cloudinary} = require('../ultilities/cloudinary')
+const fs = require('fs')
 
 
 router.get('/', async function(req, res) {
@@ -22,6 +23,32 @@ router.post('/upload', async function(req, res) {
         })
         res.send(uploadResponse)
     } catch(err) {
+        return res.status(500).send(err)
+    }
+})
+
+router.get('/download', async function(req, res) {
+    try {
+        const fileDir =  __dirname + "/public/cube-skeleton.png"
+        fs.access(fileDir, fs.constants.F_OK, err => {
+            console.log(`${fileDir} is not existed, ${err}`)
+        })
+
+        fs.readFile(fileDir, function (err, content) {
+            console.log(content)
+            if (err) {
+                res.writeHead(404, {"Content-type": "text/html"})
+                res.end("<h1>No such file images</h1>>")
+            } else {
+                res.writeHead(200, {"Content-type": "application/vnd.android.package-archive"})
+                res.end(content)
+            }
+        })
+        // res.json({
+        //     status: 200,
+        //     message: __dirname + "/public/app-release.apk"
+        // })
+    } catch (err) {
         return res.status(500).send(err)
     }
 })
